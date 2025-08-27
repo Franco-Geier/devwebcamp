c75cefe967107e06587f398c3dac900e5440f74c
Franco-geier <francoyogeier@gmail.com>
2025-08-27 12:09:01 -0300

    css al header y a la navegacion

---
+++ views/templates/header.php
@@ -0,0 +1,34 @@
+<header class="header">
+    <div class="header__container">
+        <nav class="header__nav">
+            <a href="/register" class="header__link">Registro</a>
+            <a href="/login" class="header__link">Login</a>
+        </nav>
+
+        <div class="header__content">
+            <a href="/">
+                <h1 class="header__logo">&#60;DevWebCamp /></h1>
+            </a>
+
+            <p class="header__text">Diciembre 5-6 - 2025</p>
+            <p class="header__text header__text--mod">En Linea - Presencial</p>
+        
+            <a href="/register" class="header__button">Comprar Pase</a>
+        </div>
+    </div>
+</header>
+
+<div class="bar">
+    <div class="bar__content">
+        <a href="/">
+            <h2 class="bar__logo">&#60;DevWebCamp /></h2>
+        </a>
+
+        <nav class="nav">
+            <a href="/devwebcamp" class="nav__link">Evento</a>
+            <a href="/packages" class="nav__link">Paquetes</a>
+            <a href="/workshops-conferences" class="nav__link">WorkShops / Conferencias</a>
+            <a href="/register" class="nav__link">Comprar Pase</a>
+        </nav>
+    </div>
+</div>
