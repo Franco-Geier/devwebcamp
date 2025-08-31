abaa4a0fff36b1666527f742999d7502fcbaf358
Franco-geier <francoyogeier@gmail.com>
2025-08-30 22:11:22 -0300

    Terminando footer

---
+++ views/templates/footer.php
@@ -0,0 +1,42 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__content">
+            <h3 class="footer__logo">
+                &#60;DevWebCamp />
+            </h3>
+
+            <p class="footer__text">
+                DevWebCamp es una conferencia para desarrolladores de todos los niveles,
+                se lleva a cabo de forma presencial y en línea.
+            </p>
+        </div>
+
+        <nav class="social-networks">
+            <a class="social-networks__link" rel="noopener noreferrer" target="_blank" href="https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628">
+                <span class="social-networks__hide">Facebook</span>
+            </a> 
+            <a class="social-networks__link" rel="noopener noreferrer" target="_blank" href="https://twitter.com/codigoconjuan">
+                <span class="social-networks__hide">Twitter</span>
+            </a> 
+            <a class="social-networks__link" rel="noopener noreferrer" target="_blank" href="https://youtube.com/codigoconjuan">
+                <span class="social-networks__hide">YouTube</span>
+            </a> 
+            <a class="social-networks__link" rel="noopener noreferrer" target="_blank" href="https://instagram.com/codigoconjuan">
+                <span class="social-networks__hide">Instagram</span>
+            </a> 
+            <a class="social-networks__link" rel="noopener noreferrer" target="_blank" href="https://tiktok.com/@codigoconjuan">
+                <span class="social-networks__hide">Tiktok</span>
+            </a> 
+            <a class="social-networks__link" rel="noopener noreferrer" target="_blank" href="https://github.com/codigoconjuan">
+                <span class="social-networks__hide">Github</span>
+            </a>
+        </nav>
+    </div>
+
+    <p class="footer__copyright">
+        DevWebcamp
+        <span class="footer__copyright--regular">
+            - Todos los derechos reservados <?php echo date("Y"); ?>
+        </span>
+    </p>
+</footer>
